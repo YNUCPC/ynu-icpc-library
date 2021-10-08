df19cf234e1a82d7d5b1037385e8a337abdac66c
NOSS <28915482+noss3885@users.noreply.github.com>
2021-10-08 23:08:00 +0900

    first commit

---
+++ src/template/template.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using P = pair<int,int>;
+constexpr ll MOD = 1000000007;
+const int INF = 1 << 30;
+#define REP(i,n) for(int i=0,i_len=(n); i<n; i++)
+#define ALL(v) (v).begin(), (v).end()
