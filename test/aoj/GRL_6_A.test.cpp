e1a1b040c0bf047a1e7fb683fda27acd679bac60
Kein Yukiyoshi <62263364+KY2001@users.noreply.github.com>
2021-11-01 11:02:14 +0900

    add testcase and make some revision

---
+++ test/aoj/GRL_6_A.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_A"
+#include <bits/stdc++.h>
+#include "src/flow/dinic.hpp"
+using namespace std;
+
+int main() {
+    int V, E, u, v, c;
+    cin >> V >> E;
+    Dinic flow(V);
+    for (int i = 0; i < E; i++) {
+        cin >> u >> v >> c;
+        flow.add_edge(u, v, c);
+    }
+    cout << flow.max_flow(0, V - 1) << endl;
+}
