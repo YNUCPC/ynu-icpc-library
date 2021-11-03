696c727979cd2c08a3d49d52ca5ee923c2c80212
Rogi <91960528+Rogi52@users.noreply.github.com>
2021-11-04 00:40:25 +0900

    Update GRL_5_C.test.cpp

--- test/aoj/GRL_5_C.test.cpp
+++ test/aoj/GRL_5_C.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_C"
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_5_C"
 #include <bits/stdc++.h>
 #include "src/graph/lca.hpp"
 using namespace std;
