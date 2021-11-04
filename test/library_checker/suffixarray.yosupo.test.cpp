52bdbe3a9301ac213194cb18b3ec83b037c1e34b
NOSS <28915482+noss3885@users.noreply.github.com>
2021-11-05 03:11:46 +0900

    Suffix Arrayの追加

---
+++ test/library_checker/suffixarray.yosupo.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/suffixarray"
+#include <bits/stdc++.h>
+#include "src/string/suffix_array.hpp"
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    vector<int> sa = suffix_array(s);
+    for (int i = 0; i < n; i++) {
+        cout << sa[i + 1] << " \n"[i == n];
+    }
+    return 0;
+}
