f434043393b3b81cd75844d6ad23530ee0a6cd35
Rogi <91960528+Rogi52@users.noreply.github.com>
2021-11-19 16:01:56 +0900

    Update bitwise_or_convolution.test.cpp

--- test/library_checker/bitwise_or_convolution.test.cpp
+++ test/library_checker/bitwise_or_convolution.test.cpp
@@ -6,20 +6,24 @@ using namespace std;
 
 using mint = Modint<998244353>;
 
-int main() {
+int main()
+{
     int N;
     cin >> N;
     vector<mint> a(1 << N), b(1 << N);
-    for(int i = 0; i < (1 << N); i++) cin >> a[i];
-    for(int i = 0; i < (1 << N); i++) cin >> b[i];
+    for (int i = 0; i < (1 << N); i++)
+        cin >> a[i];
+    for (int i = 0; i < (1 << N); i++)
+        cin >> b[i];
 
     // bitwise OR convolution
     reverse(a.begin(), a.end());
     reverse(b.begin(), b.end());
     vector<mint> c = Kronecker::or_convolution(a, b);
     reverse(c.begin(), c.end());
-    
-    for(int i = 0; i < (1 << N); i++) {
+
+    for (int i = 0; i < (1 << N); i++)
+    {
         cout << c[i] << " \n"[i == (1 << N) - 1];
     }
 }
