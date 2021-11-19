a7bdc6f17d7c105d9c49d0ca29e222376d9fd734
Rogi <91960528+Rogi52@users.noreply.github.com>
2021-11-19 16:42:03 +0900

    Update kronecker.hpp

--- src/math/kronecker.hpp
+++ src/math/kronecker.hpp
@@ -2,17 +2,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Kronecker
-{
+struct Kronecker {
     template <class T>
-    static void mul(vector<T> &x, T a, T b, T c, T d)
-    {
-        for (int j = 1; j < (int)x.size(); j <<= 1)
-        {
-            for (int i = 0; i < (int)x.size(); i++)
-            {
-                if ((i & j) == 0)
-                {
+    static void mul(vector<T>& x, T a, T b, T c, T d) {
+        for (int j = 1; j < (int)x.size(); j <<= 1) {
+            for (int i = 0; i < (int)x.size(); i++) {
+                if ((i & j) == 0) {
                     T s = a * x[i] + b * x[i + j];
                     T t = c * x[i] + d * x[i + j];
                     x[i] = s;
@@ -23,17 +18,15 @@ struct Kronecker
     }
 
     template <class T>
-    static void fwht(vector<T> &a, bool inv)
-    {
+    static void fwht(vector<T>& a, bool inv) {
         mul(a, T(1), T(1), T(1), T(-1));
         if (inv)
-            for (T &x : a)
+            for (T& x : a)
                 x /= T(a.size());
     }
 
     template <class T>
-    static vector<T> xor_convolution(vector<T> &a, vector<T> &b)
-    {
+    static vector<T> xor_convolution(vector<T>& a, vector<T>& b) {
         fwht(a, false);
         fwht(b, false);
         vector<T> c;
@@ -44,8 +37,7 @@ struct Kronecker
     }
 
     template <class T>
-    static vector<T> and_convolution(vector<T> &a, vector<T> &b)
-    {
+    static vector<T> and_convolution(vector<T>& a, vector<T>& b) {
         mul(a, T(1), T(1), T(0), T(1));
         mul(b, T(1), T(1), T(0), T(1));
         vector<T> c;
@@ -56,8 +48,7 @@ struct Kronecker
     }
 
     template <class T>
-    static vector<T> or_convolution(vector<T> &a, vector<T> &b)
-    {
+    static vector<T> or_convolution(vector<T>& a, vector<T>& b) {
         mul(a, T(1), T(0), T(1), T(1));
         mul(b, T(1), T(0), T(1), T(1));
         vector<T> c;
