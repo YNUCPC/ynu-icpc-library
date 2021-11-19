10996b3462724072e92ce4888e53dfc49db78004
Rogi <91960528+Rogi52@users.noreply.github.com>
2021-11-19 23:51:05 +0900

    Update test/library_checker/bitwise_xor_convolution.test.cpp
    Co-authored-by: NOSS <28915482+noss3885@users.noreply.github.com>

--- test/library_checker/bitwise_xor_convolution.test.cpp
+++ test/library_checker/bitwise_xor_convolution.test.cpp
@@ -10,10 +10,8 @@ int main() {
     int N;
     cin >> N;
     vector<mint> a(1 << N), b(1 << N);
-    for (int i = 0; i < (1 << N); i++)
-        cin >> a[i];
-    for (int i = 0; i < (1 << N); i++)
-        cin >> b[i];
+    for (int i = 0; i < (1 << N); i++) cin >> a[i];
+    for (int i = 0; i < (1 << N); i++) cin >> b[i];
     vector<mint> c = Kronecker::xor_convolution(a, b);
     for (int i = 0; i < (1 << N); i++) {
         cout << c[i] << " \n"[i == (1 << N) - 1];
