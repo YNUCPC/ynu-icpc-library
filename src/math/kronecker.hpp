17904e708c6e5547fe9533d51d5b7ea6f42bff87
Rogi <91960528+Rogi52@users.noreply.github.com>
2021-11-20 00:30:16 +0900

    namespace への変更

--- src/math/kronecker.hpp
+++ src/math/kronecker.hpp
@@ -2,60 +2,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Kronecker {
-    template <class T>
-    static void mul(vector<T>& x, T a, T b, T c, T d) {
-        int n = x.size();
-        for (int j = 1; j < n; j <<= 1) {
-            for (int i = 0; i < n; i++) {
-                if ((i & j) == 0) {
-                    T s = a * x[i] + b * x[i + j];
-                    T t = c * x[i] + d * x[i + j];
-                    x[i] = s;
-                    x[i + j] = t;
-                }
+namespace Kronecker {
+
+template <class T>
+void mul(vector<T>& x, T a, T b, T c, T d) {
+    int n = x.size();
+    for (int j = 1; j < n; j <<= 1) {
+        for (int i = 0; i < n; i++) {
+            if ((i & j) == 0) {
+                T s = a * x[i] + b * x[i + j];
+                T t = c * x[i] + d * x[i + j];
+                x[i] = s;
+                x[i + j] = t;
             }
         }
     }
+}
 
-    template <class T>
-    static void fwht(vector<T>& a, bool inv) {
-        mul(a, T(1), T(1), T(1), T(-1));
-        if (inv) {
-            for (T& x : a) x /= T(a.size());
-        }
+template <class T>
+void fwht(vector<T>& a, bool inv) {
+    mul(a, T(1), T(1), T(1), T(-1));
+    if (inv) {
+        for (T& x : a) x /= T(a.size());
     }
+}
 
-    template <class T>
-    static vector<T> xor_convolution(vector<T>& a, vector<T>& b) {
-        fwht(a, false);
-        fwht(b, false);
-        int n = a.size();
-        vector<T> c(n);
-        for (int i = 0; i < n; i++) c[i] = a[i] * b[i];
-        fwht(c, true);
-        return c;
-    }
+template <class T>
+vector<T> xor_convolution(vector<T>& a, vector<T>& b) {
+    fwht(a, false);
+    fwht(b, false);
+    int n = a.size();
+    vector<T> c(n);
+    for (int i = 0; i < n; i++) c[i] = a[i] * b[i];
+    fwht(c, true);
+    return c;
+}
 
-    template <class T>
-    static vector<T> and_convolution(vector<T>& a, vector<T>& b) {
-        mul(a, T(1), T(1), T(0), T(1));
-        mul(b, T(1), T(1), T(0), T(1));
-        int n = a.size();
-        vector<T> c(n);
-        for (int i = 0; i < n; i++) c[i] = a[i] * b[i];
-        mul(c, T(1), T(-1), T(0), T(1));
-        return c;
-    }
+template <class T>
+vector<T> and_convolution(vector<T>& a, vector<T>& b) {
+    mul(a, T(1), T(1), T(0), T(1));
+    mul(b, T(1), T(1), T(0), T(1));
+    int n = a.size();
+    vector<T> c(n);
+    for (int i = 0; i < n; i++) c[i] = a[i] * b[i];
+    mul(c, T(1), T(-1), T(0), T(1));
+    return c;
+}
 
-    template <class T>
-    static vector<T> or_convolution(vector<T>& a, vector<T>& b) {
-        mul(a, T(1), T(0), T(1), T(1));
-        mul(b, T(1), T(0), T(1), T(1));
-        int n = a.size();
-        vector<T> c(n);
-        for (int i = 0; i < n; i++) c[i] = a[i] * b[i];
-        mul(c, T(1), T(0), T(-1), T(1));
-        return c;
-    }
-};
+template <class T>
+vector<T> or_convolution(vector<T>& a, vector<T>& b) {
+    mul(a, T(1), T(0), T(1), T(1));
+    mul(b, T(1), T(0), T(1), T(1));
+    int n = a.size();
+    vector<T> c(n);
+    for (int i = 0; i < n; i++) c[i] = a[i] * b[i];
+    mul(c, T(1), T(0), T(-1), T(1));
+    return c;
+}
+
+}  // namespace Kronecker
