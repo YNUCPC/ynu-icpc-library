5e28c947cbd7ce61c3bf65e76234b64f81825018
Rogi <91960528+Rogi52@users.noreply.github.com>
2021-12-28 18:54:30 +0900

    Update fenwick_tree.hpp
    メンバ関数間の空行の削除

--- src/datastructure/fenwick_tree.hpp
+++ src/datastructure/fenwick_tree.hpp
@@ -12,14 +12,12 @@ template <class T> struct FenwickTree {
     void add(int i, T x) {
         for (int p = i + 1; p <= n; p += p & -p) data[p - 1] += x;
     }
-
     // [0, r)
     T sum(int r) {
         T s(0);
         for (int p = r; p > 0; p -= p & -p) s += data[p - 1];
         return s;
     }
-
     // [l, r)
     T sum(int l, int r) {
         return sum(r) - sum(l);
