2b3baea166a1f05fc8de979aca844626df17fd87
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-01-08 16:02:30 +0900

    add tests for segments/lines

---
+++ test/aoj/CGL_2_A.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_A"
+#include <bits/stdc++.h>
+#include "2d_segment_and_line.hpp"
+using namespace std;
+
+int main() {
+    int q; cin >> q;
+    while(q--) {
+        Segment a,b;
+        {
+            Point p0, p1; cin >> p0 >> p1;
+            a = Segment(p0, p1);
+            cin >> p0 >> p1;
+            b = Segment(p0, p1);
+        }
+        if(is_parallel(a, b)) {
+            cout << 2 << endl;
+        } else if(is_orthogonal(a, b)) {
+            cout << 1 << endl;
+        } else {
+            cout << 0 << endl;
+        }
+    }
+}
---
+++ test/aoj/CGL_2_C.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_C"
+#include <bits/stdc++.h>
+#include "2d_segment_and_line.hpp"
+using namespace std;
+
+int main() {
+    int q; cin >> q;
+    while(q--) {
+        Segment a,b;
+        {
+            Point p0, p1; cin >> p0 >> p1;
+            a = Segment(p0, p1);
+            cin >> p0 >> p1;
+            b = Segment(p0, p1);
+        }
+
+        cout << cross_point_ll(a, b) << endl;
+    }
+}
