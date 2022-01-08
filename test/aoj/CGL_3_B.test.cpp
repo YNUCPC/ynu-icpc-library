2d2c746a6955a4b65def87b237d29bd9013a6694
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-01-08 16:20:04 +0900

    Add files via upload

---
+++ test/aoj/CGL_3_B.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/3/CGL_3_B"
+#include <bits/stdc++.h>
+#include "2d_polygon.hpp"
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    Polygon p(n);
+    for (int i = 0; i < n; i++) cin >> p[i];
+    cout << is_convex(p) << endl;
+}
