a32a8901ab02ae2e3231851149ab6ee98bb035b5
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-01-08 17:00:00 +0900

    Add files via upload

---
+++ test/aoj/CGL_5_A.test.cpp
@@ -0,0 +1,11 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/5/CGL_5_A"
+#include <bits/stdc++.h>
+#include "2d_set.hpp"
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    Polygon ps(n);
+    for(int i = 0; i < n; i++) cin >> ps[i];
+    cout << fixed << setprecision(20) << closest_pair(ps) << endl;
+}
