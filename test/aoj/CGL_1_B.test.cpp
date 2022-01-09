fef255dc3fad37d2b7818294f3b6c2077751133d
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-01-09 15:18:23 +0900

    Update CGL_1_B.test.cpp

--- test/aoj/CGL_1_B.test.cpp
+++ test/aoj/CGL_1_B.test.cpp
@@ -1,4 +1,5 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/1/CGL_1_B"
+#define ERROR 0.00000001
 #include <bits/stdc++.h>
 #include "../../src/geometry/2d_geometry_template.hpp"
 #include "../../src/geometry/2d_point_and_vector.hpp"
