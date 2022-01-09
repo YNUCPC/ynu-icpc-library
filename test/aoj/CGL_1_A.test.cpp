2e1081b9cc79f01bc42a04c247592c662bef3a47
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-01-09 14:40:44 +0900

    Update CGL_1_A.test.cpp

--- test/aoj/CGL_1_A.test.cpp
+++ test/aoj/CGL_1_A.test.cpp
@@ -1,6 +1,5 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/1/CGL_1_A"
 #include <bits/stdc++.h>
-#include "../../src/geometry/2d_geometry_template.hpp"
 #include "../../src/geometry/2d_point_and_vector.hpp"
 using namespace std;
 
