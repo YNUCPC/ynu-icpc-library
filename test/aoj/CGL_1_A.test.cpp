54fcb7f6e836b26907710419da1c517ad43631e1
Rogi <91960528+Rogi52@users.noreply.github.com>
2022-01-09 15:17:56 +0900

    Update CGL_1_A.test.cpp

--- test/aoj/CGL_1_A.test.cpp
+++ test/aoj/CGL_1_A.test.cpp
@@ -1,4 +1,5 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/1/CGL_1_A"
+#define ERROR 0.00000001
 #include <bits/stdc++.h>
 #include "../../src/geometry/2d_point_and_vector.hpp"
 using namespace std;
