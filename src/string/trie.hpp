7229cde1937531f38cddd6722895d2e5f6a4c351
NOSS <28915482+noss3885@users.noreply.github.com>
2022-08-30 21:27:00 +0900

    Modify Trie

--- src/string/trie.hpp
+++ src/string/trie.hpp
@@ -8,7 +8,7 @@ struct Trie {
     static constexpr int C_BEGIN = 'a';  // C_BEGIN : 開始文字
     int root = 0;
     struct Node {
-        int child[C_SIZE];  // 子ノードの番号
+        int child[C_SIZE];  // 子ノードの番号, 存在しなければ-1
         vector<int> ids;    // そのノードが終端である文字列のIDリスト
         Node() { fill(child, child + C_SIZE, -1); }
     };
@@ -16,35 +16,32 @@ struct Trie {
   public:
     vector<Node> nodes;
     int cnt = 0;  // 追加した文字列の個数
-
     Trie() : nodes(1) {}
-    // nodes[idx]から文字cで遷移したときのindex
+    // nodes[idx]から文字cで遷移したときの頂点のindex
     int next_index(int idx, char c) {
         return nodes[idx].child[c - C_BEGIN];
     }
     // 文字列の追加
-    void add(const string& str) {
+    void insert(const string str) {
         int now = root;
         for (auto c : str) {
-            int nxt = next_index(now, c);
+            int k = c - C_BEGIN;
+            int& nxt = nodes[now].child[k];
             if (nxt == -1) {
-                nxt = int(nodes.size());
-                nodes[now].child[c - C_BEGIN] = nxt;
+                now = nxt = int(nodes.size());
                 nodes.push_back(Node());
+            } else {
+                now = nxt;
             }
-            now = nxt;
         }
-        nodes[now].ids.push_back(cnt);
-        cnt++;
+        nodes[now].ids.push_back(cnt++);
     }
-    // 文字列に対応するnodeの検索
-    int find(const string& str) {
+    // 文字列に対応するnodeのindexを検索, 存在しなければ-1
+    int find(const string str) {
         int now = root;
         for (auto c : str) {
             int nxt = next_index(now, c);
-            if (nxt == -1) {
-                return -1;
-            }
+            if (nxt == -1) return -1;
             now = nxt;
         }
         return now;
